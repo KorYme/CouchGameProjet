143bdcf77dea469f09100c11b44f99ad21f62cbe
KorYme <92892240+KorYme@users.noreply.github.com>
2023-10-18 23:49:37 +0200

    [ADD] : Google Package + [DONE] : Tool to update implementation

---
+++ PlatiniumProject/Assets/UnityGoogleDrive/Runtime/Authorization/IClientCredentials.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityGoogleDrive
+{
+    public interface IClientCredentials
+    {
+        string AuthUri { get; }
+        string TokenUri { get; }
+        string ClientId { get; }
+        string ClientSecret { get; }
+    }
+}
