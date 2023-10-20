a3bfe4abea3a7635643d1ed5c3cb3ee1de3a852f
KorYme <92892240+KorYme@users.noreply.github.com>
2023-10-20 13:52:44 +0200

    Revert "[ADD] : Begin rework Timing"
    This reverts commit e6761c7aa55116509f59cb56918f6b00275858c2.

--- PlatiniumProject/Assets/Scripts/Beats/BeatManager.cs
+++ PlatiniumProject/Assets/Scripts/Beats/BeatManager.cs
@@ -19,32 +19,31 @@ public class BeatManager : MonoBehaviour, ITimingable
     public UnityEvent OnBeatStartEvent => _onBeatStartEvent;
     public UnityEvent OnBeatEndEvent => _onBeatEndEvent;
 
-    public float TimeSinceLastBeat => Time.time - TimeOfLastBeat;
-
-    public bool IsOnBeat => TimeSinceLastBeat < _timingWindow / 2f || TimeSinceLastBeat + (_timingWindow / 2f) >= BeatDuration;
-
-    float _timeOfLastBeat;
-    public float TimeOfLastBeat => _timeOfLastBeat;
-
-    float _beatDuration;
-    public float BeatDuration => _beatDuration;
-
     [Header("Parameters"), Space]
-    [SerializeField, Tooltip("Sum of time before and after the beat for the input (in seconds)")] int _timingWindow;
+    [SerializeField] int _timingWindowInMilliseconds;
+
+    int _beatDurationInMilliseconds;
+    int _timerInMilliseconds;
+
+    public bool IsInsideBeat { get; private set; }
+
 
     private void Start()
     {
-        _beatDuration = 1;
-        //StartCoroutine(BeatCoroutine());
+        _beatDurationInMilliseconds = 1000;
+        _timerInMilliseconds = 0;
+        IsInsideBeat = true;
+        StartCoroutine(BeatCoroutine());
         _beatWwiseEvent.Post(gameObject, (uint)AkCallbackType.AK_MusicSyncBeat, BeatCallBack);
     }
 
     private void BeatCallBack(object in_cookie, AkCallbackType in_type, AkCallbackInfo in_info)
     {
+        _timerInMilliseconds = 0;
         switch (in_info)
         {
             case AkMusicSyncCallbackInfo info:
-                _beatDuration = info.segmentInfo_fBeatDuration;
+                _beatDurationInMilliseconds = (int)(info.segmentInfo_fBeatDuration * 1000);
                 break;
             default:
                 break;
@@ -52,40 +51,28 @@ public class BeatManager : MonoBehaviour, ITimingable
         _onBeatEvent?.Invoke();
     }
 
-    //IEnumerator BeatCoroutine()
-    //{
-    //    while (true)
-    //    {
-    //        _timerInMilliseconds += 20;
-    //        if (IsInsideBeat)
-    //        {
-    //            if (_timerInMilliseconds >= _timingWindowInMilliseconds / 2 && _beatDurationInMilliseconds - _timerInMilliseconds > _timingWindowInMilliseconds / 2)
-    //            {
-    //                IsInsideBeat = false;
-    //                _onBeatEndEvent?.Invoke();
-    //            }
-    //        }
-    //        else
-    //        {
-    //            if (_beatDurationInMilliseconds - _timerInMilliseconds <= _timingWindowInMilliseconds / 2)
-    //            {
-    //                IsInsideBeat = true;
-    //                _onBeatStartEvent?.Invoke();
-    //            }
-    //        }
-    //        yield return new WaitForFixedUpdate();
-    //    }
-    //}
-
-    IEnumerator EventBeatCoroutine()
+    IEnumerator BeatCoroutine()
     {
         while (true)
         {
-            yield return new WaitWhile(() => !IsOnBeat);
-            OnBeatEndEvent?.Invoke();
-            yield return new WaitWhile(() => IsOnBeat);
-            OnBeatStartEvent?.Invoke();
+            _timerInMilliseconds += 20;
+            if (IsInsideBeat)
+            {
+                if (_timerInMilliseconds >= _timingWindowInMilliseconds / 2 && _beatDurationInMilliseconds - _timerInMilliseconds > _timingWindowInMilliseconds / 2)
+                {
+                    IsInsideBeat = false;
+                    _onBeatEndEvent?.Invoke();
+                }
+            }
+            else
+            {
+                if (_beatDurationInMilliseconds - _timerInMilliseconds <= _timingWindowInMilliseconds / 2)
+                {
+                    IsInsideBeat = true;
+                    _onBeatStartEvent?.Invoke();
+                }
+            }
+            yield return new WaitForFixedUpdate();
         }
-        yield return null;
     }
 }
--- PlatiniumProject/Assets/Scripts/Beats/ITimingable.cs
+++ PlatiniumProject/Assets/Scripts/Beats/ITimingable.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Rendering;
 
 public interface ITimingable
 {
-    public float BeatDuration { get; }
-    public float TimeOfLastBeat { get; }
-    public float TimeSinceLastBeat { get; }
-    public bool IsOnBeat { get; }
+    public bool IsInsideBeat { get; }
     public UnityEvent OnBeatEvent { get; }
     public UnityEvent OnBeatStartEvent { get; }
     public UnityEvent OnBeatEndEvent { get; }
