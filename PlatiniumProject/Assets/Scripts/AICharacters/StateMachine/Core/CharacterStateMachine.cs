f994b60a2820347a4775dd63c7a2df97c7606d1d
Chloe-Ms <mllechloe.mathias@gmail.com>
2023-11-03 15:24:44 +0100

    [ADD] Evil characters in QTE DJ

--- PlatiniumProject/Assets/Scripts/AICharacters/StateMachine/Core/CharacterStateMachine.cs
+++ PlatiniumProject/Assets/Scripts/AICharacters/StateMachine/Core/CharacterStateMachine.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using Random = UnityEngine.Random;
 
 public class CharacterStateMachine : MonoBehaviour
