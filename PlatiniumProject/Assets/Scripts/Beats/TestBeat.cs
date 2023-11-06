482dfe7872849e0e72ba01bdbeff59757f957db9
KorYme <92892240+KorYme@users.noreply.github.com>
2023-11-06 10:53:21 +0100

    [ADD] : Scene to find potentiel offset on music

--- PlatiniumProject/Assets/Scripts/Beats/TestBeat.cs
+++ PlatiniumProject/Assets/Scripts/Beats/TestBeat.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class TestBeat : MonoBehaviour
 {
+    [SerializeField, Range(0f, 1f)] float _offsetTest;
     [SerializeField] SpriteRenderer _spriteRenderer;
 
     DateTime _lastBeatTiming;
+    Coroutine _offsetCoroutine;
 
     private void Reset()
     {
@@ -23,7 +25,7 @@ public class TestBeat : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log($"Timing between last input : {_lastBeatTiming}ms");
+            Debug.Log($"Timing between last input : {_lastBeatTiming.Millisecond}ms");
             _lastBeatTiming = DateTime.Now;
         }
     }
@@ -41,6 +43,25 @@ public class TestBeat : MonoBehaviour
     public void ChangeColorToGreen()
     {
         _spriteRenderer.color = Color.green;
+    }
+
+    public void ChangeColorWithTimer()
+    {
+        Debug.Log("Beat");
+        if (_offsetCoroutine != null)
+        {
+            StopCoroutine(_offsetCoroutine);
+            _spriteRenderer.color = Color.red;
+        }
+        _offsetCoroutine = StartCoroutine(WaitForOffset());
+    }
+
+    IEnumerator WaitForOffset()
+    {
+        yield return new WaitForSeconds(_offsetTest);
+        ChangeColorToGreen();
+        yield return new WaitForSeconds(.15f);
+        ChangeColorToRed();
     }
 
     public void Print(string text)
