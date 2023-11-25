fb4ceb126225a8e3d7bd67f0ecace8aefe2e9203
SoleneFalk <solene.falk@gmail.com>
2023-11-25 20:22:32 +0100

    Mise en place système musique Wwise + génération soundbank

--- PlatiniumProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ PlatiniumProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,7 +17,72 @@ namespace AK
         static const AkUniqueID MUSIC_MAGE = 1716433435U;
         static const AkUniqueID MUSIC_METRONOME = 1823217099U;
         static const AkUniqueID MUSIC_PAYDAY = 3926639385U;
+        static const AkUniqueID PLAY_BARMANFAIL = 1537570307U;
+        static const AkUniqueID PLAY_BARMANMOVEMENT = 293490316U;
+        static const AkUniqueID PLAY_BARMANSUCCESS = 3367986140U;
+        static const AkUniqueID PLAY_BOUNCERACCEPT = 405290892U;
+        static const AkUniqueID PLAY_BOUNCERCHECK = 3855599980U;
+        static const AkUniqueID PLAY_BOUNCERDECLINE = 2572635194U;
+        static const AkUniqueID PLAY_BOUNCERFAIL = 585628320U;
+        static const AkUniqueID PLAY_BOUNCERMOVEMENT = 1590991995U;
+        static const AkUniqueID PLAY_BOUNCERSUCCESS = 1068381885U;
+        static const AkUniqueID PLAY_DJFAIL = 515099352U;
+        static const AkUniqueID PLAY_DJSUCCESS = 3032600805U;
+        static const AkUniqueID PLAY_DROPEND = 681292744U;
+        static const AkUniqueID PLAY_DROPFAIL = 1105172925U;
+        static const AkUniqueID PLAY_DROPSUCCESS = 2682578994U;
+        static const AkUniqueID PLAY_DROPTRIGGERING = 2949936265U;
+        static const AkUniqueID PLAY_GAMEOVER = 3174629258U;
+        static const AkUniqueID PLAY_MENUCLICK = 2576276897U;
+        static const AkUniqueID PLAY_MENUCLOSE = 2709512125U;
+        static const AkUniqueID PLAY_MENUHOVER = 599303179U;
+        static const AkUniqueID PLAY_MENUOPEN = 119142231U;
+        static const AkUniqueID PLAY_MUSIC_DND = 3388161964U;
+        static const AkUniqueID PLAY_NEWGAME = 1215962870U;
+        static const AkUniqueID PLAY_VOLUMEBAR = 1570308127U;
+        static const AkUniqueID SET_STATE_DROP_1 = 2391209851U;
+        static const AkUniqueID SET_STATE_DROP_2 = 2391209848U;
+        static const AkUniqueID SET_STATE_DROP_3 = 2391209849U;
+        static const AkUniqueID SET_STATE_MUSIQUE_1 = 389274865U;
+        static const AkUniqueID SET_STATE_MUSIQUE_2 = 389274866U;
+        static const AkUniqueID SET_STATE_MUSIQUE_3 = 389274867U;
+        static const AkUniqueID SET_STATE_MUSIQUE_END = 2226536039U;
+        static const AkUniqueID SET_STATE_MUSIQUE_INTRO = 3234236150U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LOWPASS_MENU
+        {
+            static const AkUniqueID GROUP = 2450363656U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MENU_CLOSE = 626954121U;
+                static const AkUniqueID MENU_OPEN = 1287511387U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace LOWPASS_MENU
+
+        namespace MUSIC_STATE
+        {
+            static const AkUniqueID GROUP = 3826569560U;
+
+            namespace STATE
+            {
+                static const AkUniqueID DROP_1 = 3749925386U;
+                static const AkUniqueID DROP_2 = 3749925385U;
+                static const AkUniqueID DROP_3 = 3749925384U;
+                static const AkUniqueID MUSIQUE_1 = 3094479182U;
+                static const AkUniqueID MUSIQUE_2 = 3094479181U;
+                static const AkUniqueID MUSIQUE_3 = 3094479180U;
+                static const AkUniqueID MUSIQUE_END = 1803299556U;
+                static const AkUniqueID MUSIQUE_INTRO = 3387179977U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE
+
+    } // namespace STATES
 
     namespace BANKS
     {
