fe66b2d4078bad91d1178b30963b00f8fc4af906
SoleneFalk <solene.falk@gmail.com>
2023-12-12 11:32:19 +0100

    Ajout stingers réussite et défaite + loops intro et end + bus de mix musique/SFX avec ducking

--- PlatiniumProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ PlatiniumProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -146,7 +146,12 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID CHARACTER = 436743010U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
