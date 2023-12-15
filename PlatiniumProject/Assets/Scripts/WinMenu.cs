85be5253d7abdda8c1eab5fc54845dab842dfd11
Tsem12 <82233249+Tsem12@users.noreply.github.com>
2023-12-15 18:16:11 +0100

    [ADD] particule rendered in textures

--- PlatiniumProject/Assets/Scripts/WinMenu.cs
+++ PlatiniumProject/Assets/Scripts/WinMenu.cs
@@ -1,22 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Sequence = Unity.VisualScripting.Sequence;
 
 public class WinMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _winMenu;
+    [SerializeField] private RectTransform _winMenu;
     [SerializeField] private TMP_Text _winText;
 
     [SerializeField] private float _timeToDisplaySacrifises;
     [SerializeField] private AnimationCurve _displayCurve;
 
+    [SerializeField] private AK.Wwise.Event _incremetation;
+    
+    [SerializeField] private Transform _flame;
+    [SerializeField] private Vector2 _flameMinMaxPos;
+    [SerializeField] private int _clientMaxAmountForFlames;
+    public int DebugClient;
+
     private int _actualScore;
 
     private Coroutine _displayRoutine;
 
-    public int SacrifiedClient { get; set; } = 50;
+    public int SacrifiedClient { get; set; }
 
     private void Awake()
     {
@@ -35,31 +46,44 @@ public class WinMenu : MonoBehaviour
 
     private void DisplayWinMenu()
     {
-        _winMenu.SetActive(true);
+        _winMenu.gameObject.SetActive(true);
         _displayRoutine = StartCoroutine(DisplaySacrificeRoutine());
     }
 
     IEnumerator DisplaySacrificeRoutine()
     {
+        DG.Tweening.Sequence sequence = DOTween.Sequence();
+        sequence.Join(_winMenu.DOScale(Vector3.one * 1.05f, (float)Globals.BeatManager.BeatDurationInSeconds/2));
+        sequence.SetLoops(-1, LoopType.Yoyo);
         _winText.text = _winText.text.Replace("NANANA", "0");
         _actualScore = 0;
         float timer = 0f;
         float percentage = 0f;
-        //yield return new WaitForSeconds(0.5f);
         while (timer < _timeToDisplaySacrifises)
         {
             timer += Time.deltaTime;
             percentage = _displayCurve.Evaluate(timer / _timeToDisplaySacrifises);
             _winText.text = _winText.text.Replace(_actualScore.ToString(), ((int)(SacrifiedClient * percentage)).ToString());
+            if (_actualScore != (int)(SacrifiedClient * percentage))
+            {
+                _incremetation.Post(gameObject);
+            }
             _actualScore = (int)(SacrifiedClient * percentage);
+            _flame.localPosition = new Vector3(0, Mathf.Lerp(_flameMinMaxPos.x, _flameMinMaxPos.y, Mathf.Clamp01((_actualScore / (float)_clientMaxAmountForFlames))), 0);
             yield return null;
         }
+
+        // yield return new WaitForSeconds(2f);
+        // sequence.Kill();
+        // _winMenu.gameObject.SetActive(false);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
+            _actualScore = 0;
+            SacrifiedClient = DebugClient;
             DisplayWinMenu();
         }
     }
